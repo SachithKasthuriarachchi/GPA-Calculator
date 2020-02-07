665d046d5540441ffca8f062732d6361a963ec49
SachithKasthuriarachchi <sac.nil.kas@gmail.com>
2020-02-07 20:48:55 +0530

    Correcting packaging errors due to htmlunit

--- app/src/main/java/com/sachith/gpacalculator/HtmlHandler.java
+++ app/src/main/java/com/sachith/gpacalculator/HtmlHandler.java
@@ -2,47 +2,107 @@ package com.sachith.gpacalculator;
 
 import android.os.AsyncTask;
 
-import org.jsoup.Connection;
+import com.android.gargoylesoftware.htmlunit.WebClient;
+import com.android.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.android.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.android.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
-import java.io.IOException;
-import java.net.URLEncoder;
-import java.util.Map;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class HtmlHandler extends AsyncTask<String, Void, String> {
 
 
     @Override
-    protected String doInBackground(String... urls) {
-
+    protected String doInBackground(String... strings) {
         try {
-            String login = URLEncoder.encode("Login", "UTF-8");
-            String name = URLEncoder.encode("160290R", "UTF-8");
-            String pass = URLEncoder.encode("SN1Math$lifE23", "UTF-8");
-            Connection.Response res = Jsoup.connect("https://lms.mrt.ac.lk/enrolments.php")
-                    .data(URLEncoder.encode("LearnOrgUsername", "UTF-8"), name,
-                            URLEncoder.encode("LearnOrgPassword", "UTF-8"), pass,
-                            URLEncoder.encode("LearnOrgLogin", "UTF-8"), login).header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
-                    .header("Accept-Encoding", "gzip, deflate, br")
-                    .header("Accept-Language", "en-US,en;q=0.5")
-                    .header("Connection", "keep-alive")
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .header("Host", "lms.mrt.ac.lk")
-                    .header("Origin", "https://lms.mrt.ac.lk")
-                    .header("Referer", "https://lms.mrt.ac.lk/login_index.php")
-                    .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0").method(Connection.Method.POST)
-                    .execute();
-
-            Map<String, String > loginCookies = res.cookies();
-
-            Document document = Jsoup.connect("https://lms.mrt.ac.lk/enrolments.php")
-                    .cookies(loginCookies).get();
-
-            return  document.outerHtml();
-        } catch (IOException e) {
+            String result = GET(strings[0], strings[1]);
+            System.out.println(result);
+        } catch (Exception e) {
             e.printStackTrace();
-            return "";
         }
+        return null;
+    }
+
+    private static WebClient LogIn(WebClient webClient, String uname, String pwd) throws Exception {
+
+        //webClient.getOptions().setJavaScriptEnabled(false);
+        HtmlPage currentPage = webClient.getPage("https://lms.mrt.ac.lk/enrolments.php"); //Load page at the STRING address.
+        HtmlInput username = currentPage.getElementByName("LearnOrgUsername"); //Find element called loginuser for username
+        username.setValueAttribute(uname); //Set value for username
+        HtmlInput password = currentPage.getElementByName("LearnOrgPassword"); //Find element called loginpassword for password
+        password.setValueAttribute(pwd); //Set value for password
+        HtmlSubmitInput submitBtn = currentPage.getElementByName("LearnOrgLogin"); //Find element called Submit to submit form.
+        currentPage = submitBtn.click(); //Click on the button.
+
+        return webClient;
+    }
+
+    private String GET(String username, String password) throws Exception {
+
+        WebClient webClient = new WebClient();
+        webClient = LogIn(webClient, username, password);
+
+        HtmlPage currentPage = webClient.getPage("http://lms.mrt.ac.lk/enrolments.php");
+        String pageSource = currentPage.asXml();
+
+        Document document = Jsoup.parse(pageSource);
+        System.out.println(document);
+        JSONObject object = new JSONObject();
+        object.put("Semester_1", new JSONArray());
+        object.put("Semester_2", new JSONArray());
+        object.put("Semester_3", new JSONArray());
+        object.put("Semester_4", new JSONArray());
+        object.put("Semester_5", new JSONArray());
+        object.put("Semester_6", new JSONArray());
+        object.put("Semester_7", new JSONArray());
+        object.put("Semester_8", new JSONArray());
+
+        Element table = document.select("tbody").last();
+        Elements row = table.getElementsByTag("tr");
+
+        for (int i = 2; i < row.size(); i++) {
+
+            Elements col = row.eq(i).select("td");
+
+            if (col.size() == 4) {
+                String semester = col.get(0).text();
+                String code = col.get(1).text();
+                String module = col.get(2).text();
+                String credits = col.get(3).text();
+                JSONObject modObject = new JSONObject();
+                modObject.put("name", module);
+                modObject.put("credits", credits);
+
+                JSONObject cObject = new JSONObject();
+                cObject.put(code, modObject);
+
+
+                if (semester.contains("Semester - 1")) {
+                    ((JSONArray) object.get("Semester_1")).put(cObject);
+                } else if (semester.contains("Semester - 2")) {
+                    ((JSONArray) object.get("Semester_2")).put(cObject);
+                } else if (semester.contains("Semester - 3")) {
+                    ((JSONArray) object.get("Semester_3")).put(cObject);
+                } else if (semester.contains("Semester - 4")) {
+                    ((JSONArray) object.get("Semester_4")).put(cObject);
+                } else if (semester.contains("Semester - 5")) {
+                    ((JSONArray) object.get("Semester_5")).put(cObject);
+                } else if (semester.contains("Semester - 6")) {
+                    ((JSONArray) object.get("Semester_6")).put(cObject);
+                } else if (semester.contains("Semester - 7")) {
+                    ((JSONArray) object.get("Semester_7")).put(cObject);
+                } else if (semester.contains("Semester - 8")) {
+                    ((JSONArray) object.get("Semester_8")).put(cObject);
+                }
+                //System.out.println(cObject.toString());
+            }
+        }
+
+        return object.toString();
     }
 }
--- app/src/main/java/com/sachith/gpacalculator/MainActivity.java
+++ app/src/main/java/com/sachith/gpacalculator/MainActivity.java
@@ -7,10 +7,10 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlInput;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.android.gargoylesoftware.htmlunit.WebClient;
+import com.android.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.android.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.android.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -39,13 +39,15 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String n = name.getText().toString();
                 String p = password.getText().toString();
-                String result = null;
+                String[] gf = {n,p};
+
+                new HtmlHandler().execute(n,p);
                 try {
-                    result = GET();
+                    //result = GET();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                System.out.println(result);
+                //System.out.println(result);
 
 
                 //WebDriver driver = new AndroidWebDriver()
@@ -56,81 +58,5 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private static WebClient LogIn(WebClient webClient) throws Exception {
 
-        //webClient.getOptions().setJavaScriptEnabled(false);
-        HtmlPage currentPage = webClient.getPage("https://lms.mrt.ac.lk/enrolments.php"); //Load page at the STRING address.
-        HtmlInput username = currentPage.getElementByName("LearnOrgUsername"); //Find element called loginuser for username
-        username.setValueAttribute("160290R"); //Set value for username
-        HtmlInput password = currentPage.getElementByName("LearnOrgPassword"); //Find element called loginpassword for password
-        password.setValueAttribute("SN1Math$lifE23"); //Set value for password
-        HtmlSubmitInput submitBtn = currentPage.getElementByName("LearnOrgLogin"); //Find element called Submit to submit form.
-        currentPage = submitBtn.click(); //Click on the button.
-
-        return webClient;
-    }
-
-    private String GET() throws Exception {
-
-        WebClient webClient = new WebClient();
-        webClient = LogIn(webClient);
-
-        HtmlPage currentPage = webClient.getPage("https://lms.mrt.ac.lk/enrolments.php");
-        String pageSource = currentPage.asXml();
-
-        Document document = Jsoup.parse(pageSource);
-        System.out.println(document);
-        JSONObject object = new JSONObject();
-        object.put("Semester_1", new JSONArray());
-        object.put("Semester_2", new JSONArray());
-        object.put("Semester_3", new JSONArray());
-        object.put("Semester_4", new JSONArray());
-        object.put("Semester_5", new JSONArray());
-        object.put("Semester_6", new JSONArray());
-        object.put("Semester_7", new JSONArray());
-        object.put("Semester_8", new JSONArray());
-
-        Element table = document.select("tbody").last();
-        Elements row = table.getElementsByTag("tr");
-
-        for (int i = 2; i < row.size(); i++) {
-
-            Elements col = row.eq(i).select("td");
-
-            if (col.size() == 4) {
-                String semester = col.get(0).text();
-                String code = col.get(1).text();
-                String module = col.get(2).text();
-                String credits = col.get(3).text();
-                JSONObject modObject = new JSONObject();
-                modObject.put("name", module);
-                modObject.put("credits", credits);
-
-                JSONObject cObject = new JSONObject();
-                cObject.put(code, modObject);
-
-
-                if (semester.contains("Semester - 1")) {
-                    ((JSONArray) object.get("Semester_1")).put(cObject);
-                } else if (semester.contains("Semester - 2")) {
-                    ((JSONArray) object.get("Semester_2")).put(cObject);
-                } else if (semester.contains("Semester - 3")) {
-                    ((JSONArray) object.get("Semester_3")).put(cObject);
-                } else if (semester.contains("Semester - 4")) {
-                    ((JSONArray) object.get("Semester_4")).put(cObject);
-                } else if (semester.contains("Semester - 5")) {
-                    ((JSONArray) object.get("Semester_5")).put(cObject);
-                } else if (semester.contains("Semester - 6")) {
-                    ((JSONArray) object.get("Semester_6")).put(cObject);
-                } else if (semester.contains("Semester - 7")) {
-                    ((JSONArray) object.get("Semester_7")).put(cObject);
-                } else if (semester.contains("Semester - 8")) {
-                    ((JSONArray) object.get("Semester_8")).put(cObject);
-                }
-                //System.out.println(cObject.toString());
-            }
-        }
-
-        return object.toString();
-    }
 }
