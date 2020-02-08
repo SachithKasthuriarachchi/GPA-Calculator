d2e84b79a44990f298d48cba1d10efda183f32e7
SachithKasthuriarachchi <sac.nil.kas@gmail.com>
2020-02-08 17:37:19 +0530

    Adding the semester, credits and gpa columns to the "users" table

--- app/src/main/java/com/sachith/gpacalculator/UserDBHelper.java
+++ app/src/main/java/com/sachith/gpacalculator/UserDBHelper.java
@@ -9,7 +9,10 @@ public class UserDBHelper extends SQLiteOpenHelper {
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + UserReaderDB.UserEntry.TABLE_NAME + " (" +
                     UserReaderDB.UserEntry._ID + " INTEGER PRIMARY KEY," +
-                    UserReaderDB.UserEntry.COLUMN_NAME_INDEX + " TEXT)";
+                    UserReaderDB.UserEntry.COLUMN_NAME_INDEX + " TEXT," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER + " INTEGER," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_CREDITS + " REAL," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_GPA + " REAL)";
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + UserReaderDB.UserEntry.TABLE_NAME;
