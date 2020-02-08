6ca2109a4aa3296f50a5492ef98f43c11e096336
SachithKasthuriarachchi <sac.nil.kas@gmail.com>
2020-02-08 17:36:34 +0530

    Saving the credits and gpa values to a database

--- app/src/main/java/com/sachith/gpacalculator/ShowResult.java
+++ app/src/main/java/com/sachith/gpacalculator/ShowResult.java
@@ -1,12 +1,17 @@
 package com.sachith.gpacalculator;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShowResult extends AppCompatActivity {
 
@@ -22,9 +27,81 @@ public class ShowResult extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         String myGPA = format.format(bundle.getDouble("GPA"));
-        System.out.println(getIntent().getStringExtra("Index"));
+        double credits = bundle.getDouble("Credits");
+        String semDept = intent.getStringExtra("dept");
+        int semester = Integer.parseInt(semDept.substring(semDept.length() - 1));
+        System.out.println(myGPA);
+        System.out.println(credits);
+        System.out.println(semDept);
+        System.out.println(semester);
+        updateDB(intent.getStringExtra("Index"), semester, Double.parseDouble(myGPA),
+                credits);
 
         gpa.setText(myGPA);
 
+    }
+
+    private boolean searchForQuery(String index, int semester, UserDBHelper dbHelper) {
+
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER
+        };
+
+        String selection = UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER + " = ? AND " +
+                UserReaderDB.UserEntry.COLUMN_NAME_INDEX + " =?";
+        String[] selectionArgs = {String.valueOf(semester), index};
+
+        Cursor cursor = database.query(
+                UserReaderDB.UserEntry.TABLE_NAME,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                null
+        );
+        List<Integer> semIDs = new ArrayList<Integer>();
+        while(cursor.moveToNext()) {
+            int itemId = cursor.getInt(
+                    cursor.getColumnIndexOrThrow(UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER));
+            semIDs.add(itemId);
+        }
+        cursor.close();
+
+        return semIDs.size() != 0;
+
+    }
+
+    private void updateDB(String index, int semester, double gpa, double credits) {
+
+        UserDBHelper dbHelper = new UserDBHelper(getApplicationContext());
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        if (searchForQuery(index, semester, dbHelper)) {
+
+            ContentValues values =new ContentValues();
+            values.put(UserReaderDB.UserEntry.COLUMN_NAME_CREDITS, credits);
+            values.put(UserReaderDB.UserEntry.COLUMN_NAME_GPA, gpa);
+
+            String selection = UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER + " LIKE ?";
+            String[] selectionArgs = {String.valueOf(semester)};
+
+            database.update(
+                    UserReaderDB.UserEntry.TABLE_NAME,
+                    values,
+                    selection,
+                    selectionArgs
+            );
+        } else {
+
+            ContentValues values =new ContentValues();
+            values.put(UserReaderDB.UserEntry.COLUMN_NAME_INDEX, index);
+            values.put(UserReaderDB.UserEntry.COLUMN_NAME_CREDITS, credits);
+            values.put(UserReaderDB.UserEntry.COLUMN_NAME_GPA, gpa);
+            values.put(UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER, semester);
+            database.insert(UserReaderDB.UserEntry.TABLE_NAME, null, values);
+        }
     }
 }
