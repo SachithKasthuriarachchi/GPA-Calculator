92ba0e417f5ec2313eaa6d59c6eae1662d9213dd
SachithKasthuriarachchi <sac.nil.kas@gmail.com>
2020-04-25 14:39:40 +0530

    Displaying the added modules

--- app/src/main/java/com/sachith/gpacalculator/impl/DisplayModuleActivityDept.java
+++ app/src/main/java/com/sachith/gpacalculator/impl/DisplayModuleActivityDept.java
@@ -11,15 +11,21 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import com.sachith.gpacalculator.AddModuleActivity;
 import com.sachith.gpacalculator.DatabaseHelper;
 import com.sachith.gpacalculator.DisplayCalculatorActivity;
 import com.sachith.gpacalculator.DisplayModules;
+import com.sachith.gpacalculator.DisplaySemesters;
 import com.sachith.gpacalculator.R;
+import com.sachith.gpacalculator.UserDBHelper;
+import com.sachith.gpacalculator.UserReaderDB;
 import com.sachith.gpacalculator.adapter.ModuleAdapter;
 import com.sachith.gpacalculator.model.Module;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class DisplayModuleActivityDept extends AppCompatActivity implements DisplayModules {
 
@@ -27,12 +33,16 @@ public class DisplayModuleActivityDept extends AppCompatActivity implements Disp
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_modules);
-        Intent intent = getIntent();
+        final Intent intent = getIntent();
         final String department = intent.getStringExtra("Department");
         final String semester = intent.getStringExtra("Semester");
         String tableName = department + semester;
-        ArrayList<Module> allModules = getAllModules("MODULES.db", tableName);
-        final ModuleAdapter moduleAdapter = new ModuleAdapter(this, allModules);
+        ArrayList<Module> allModules = getAllModules("MODULES.db", tableName,
+                Integer.parseInt(semester.substring(semester.length() - 1)));
+        final ModuleAdapter moduleAdapter = new ModuleAdapter(this, allModules,
+                semester,
+                intent.getStringExtra("Index"),
+                department);
         ListView listView = findViewById(R.id.listViewMod);
 
         /**
@@ -41,6 +51,19 @@ public class DisplayModuleActivityDept extends AppCompatActivity implements Disp
         ListView.LayoutParams params = new ListView.LayoutParams(ListView.LayoutParams.MATCH_PARENT,
                 ListView.LayoutParams.WRAP_CONTENT
         );
+
+        CardView addModule = findViewById(R.id.addMod);
+        addModule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(v.getContext(), AddModuleActivity.class);
+                intent1.putExtra("Department", department);
+                intent1.putExtra("Semester", semester);
+                intent1.putExtra("Index", getIntent().getStringExtra("Index"));
+                intent1.putExtra("Class", "DisplayModuleActivityDept");
+                startActivity(intent1);
+            }
+        });
 
         Button button = new Button(this);
         button.setLayoutParams(params);
@@ -78,7 +101,15 @@ public class DisplayModuleActivityDept extends AppCompatActivity implements Disp
     }
 
     @Override
-    public ArrayList<Module> getAllModules(String dbName, String tableName) {
+    public void onBackPressed() {
+        Intent intent = new Intent(this, DisplaySemesters.class);
+        intent.putExtra("Department", getIntent().getStringExtra("Department"));
+        intent.putExtra("Index", getIntent().getStringExtra("Index"));
+        startActivity(intent);
+    }
+
+    @Override
+    public ArrayList<Module> getAllModules(String dbName, String tableName, int semester) {
 
         DatabaseHelper helper = new DatabaseHelper(this, dbName);
         SQLiteDatabase database = helper.getReadableDatabase();
@@ -99,6 +130,72 @@ public class DisplayModuleActivityDept extends AppCompatActivity implements Disp
             database.close();
             helper.close();
         }
-        return allModules;
+
+        return add(getAddedModules(semester), allModules);
+    }
+
+    private ArrayList<Module> add(ArrayList<Module> addedModules, ArrayList<Module> dbModules) {
+
+        for (int i = 0; i < addedModules.size(); i++) {
+            for (int j = 0; j < dbModules.size(); j++) {
+                if (addedModules.get(i).getName().equals(dbModules.get(j).getName())) {
+                    dbModules.remove(j);
+                }
+            }
+        }
+        addedModules.addAll(dbModules);
+        ArrayList<Module> result = new ArrayList<Module>(new HashSet<Module>(addedModules));
+        return result;
+    }
+
+    private ArrayList<Module> getAddedModules(int semester) {
+        Intent intent = getIntent();
+        String index = intent.getStringExtra("Index");
+        String department = intent.getStringExtra("Department");
+
+        UserDBHelper dbHelper = new UserDBHelper(getApplicationContext());
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                UserReaderDB.UserEntry.COLUMN_NAME_MODULE_NAME,
+                UserReaderDB.UserEntry.COLUMN_NAME_MODULE_CODE,
+                UserReaderDB.UserEntry.COLUMN_NAME_CREDITS
+        };
+
+        String selection = UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER + " = ? AND " +
+                UserReaderDB.UserEntry.COLUMN_NAME_DEPARTMENT + " = ? AND " +
+                UserReaderDB.UserEntry.COLUMN_NAME_INDEX + " =?";
+        String[] selectionArgs = {String.valueOf(semester), department, index};
+        String sortOrder = UserReaderDB.UserEntry.COLUMN_NAME_MODULE_NAME + " DESC";
+
+        Cursor cursor = database.query(
+                UserReaderDB.UserEntry.TABLE_NAME_MODULE,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+
+        ArrayList<Module> addedModules = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            String moduleName = cursor.getString(
+                    cursor.getColumnIndexOrThrow(UserReaderDB.UserEntry.COLUMN_NAME_MODULE_NAME)
+            );
+
+            String moduleCode = cursor.getString(
+                    cursor.getColumnIndexOrThrow(UserReaderDB.UserEntry.COLUMN_NAME_MODULE_CODE)
+            );
+
+            double credits = cursor.getDouble(
+                    cursor.getColumnIndexOrThrow(UserReaderDB.UserEntry.COLUMN_NAME_CREDITS)
+            );
+            Module module = new Module(moduleCode, moduleName, credits);
+            addedModules.add(module);
+        }
+        cursor.close();
+        return addedModules;
     }
 }
--- app/src/main/java/com/sachith/gpacalculator/impl/DisplayModuleActivityFreshers.java
+++ app/src/main/java/com/sachith/gpacalculator/impl/DisplayModuleActivityFreshers.java
@@ -11,15 +11,21 @@ import android.widget.Button;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import com.sachith.gpacalculator.AddModuleActivity;
 import com.sachith.gpacalculator.DatabaseHelper;
 import com.sachith.gpacalculator.DisplayCalculatorActivity;
+import com.sachith.gpacalculator.DisplayDeptActivity;
 import com.sachith.gpacalculator.DisplayModules;
 import com.sachith.gpacalculator.R;
+import com.sachith.gpacalculator.UserDBHelper;
+import com.sachith.gpacalculator.UserReaderDB;
 import com.sachith.gpacalculator.adapter.ModuleAdapter;
 import com.sachith.gpacalculator.model.Module;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class DisplayModuleActivityFreshers extends AppCompatActivity implements DisplayModules {
 
@@ -28,8 +34,11 @@ public class DisplayModuleActivityFreshers extends AppCompatActivity implements 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.display_modules);
 
-        ArrayList<Module> allModules = getAllModules("MODULES.db", "FIRSTSEM");
-        final ModuleAdapter moduleAdapter = new ModuleAdapter(this, allModules);
+        ArrayList<Module> allModules = getAllModules("MODULES.db", "FIRSTSEM", 1);
+        final ModuleAdapter moduleAdapter = new ModuleAdapter(this, allModules,
+                "1",
+                getIntent().getStringExtra("Index"),
+                "Fresher");
         ListView listView = (ListView) findViewById(R.id.listViewMod);
 
         /**
@@ -38,6 +47,19 @@ public class DisplayModuleActivityFreshers extends AppCompatActivity implements 
         ListView.LayoutParams params = new ListView.LayoutParams(ListView.LayoutParams.MATCH_PARENT,
                 ListView.LayoutParams.WRAP_CONTENT
         );
+
+        CardView addModule = findViewById(R.id.addMod);
+        addModule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent(v.getContext(), AddModuleActivity.class);
+                intent1.putExtra("Department", "Fresher");
+                intent1.putExtra("Semester", "1");
+                intent1.putExtra("Index", getIntent().getStringExtra("Index"));
+                intent1.putExtra("Class", "DisplayModuleActivityFreshers");
+                startActivity(intent1);
+            }
+        });
 
         Button button = new Button(this);
         button.setLayoutParams(params);
@@ -71,8 +93,14 @@ public class DisplayModuleActivityFreshers extends AppCompatActivity implements 
         });
     }
 
+    public void onBackPressed() {
+        Intent intent = new Intent(this, DisplayDeptActivity.class);
+        intent.putExtra("Index", getIntent().getStringExtra("Index"));
+        startActivity(intent);
+    }
+
     @Override
-    public ArrayList<Module> getAllModules(String dbName, String tableName) {
+    public ArrayList<Module> getAllModules(String dbName, String tableName, int semester) {
 
         DatabaseHelper helper = new DatabaseHelper(this, dbName);
         SQLiteDatabase database = helper.getReadableDatabase();
@@ -93,6 +121,71 @@ public class DisplayModuleActivityFreshers extends AppCompatActivity implements 
             database.close();
             helper.close();
         }
-        return allModules;
+        return add(getAddedModules(), allModules);
+    }
+
+    private ArrayList<Module> add(ArrayList<Module> addedModules, ArrayList<Module> dbModules) {
+
+        for (int i = 0; i < addedModules.size(); i++) {
+            for (int j = 0; j < dbModules.size(); j++) {
+                if (addedModules.get(i).getName().equals(dbModules.get(j).getName())) {
+                    dbModules.remove(j);
+                }
+            }
+        }
+        addedModules.addAll(dbModules);
+        ArrayList<Module> result = new ArrayList<Module>(new HashSet<Module>(addedModules));
+        return result;
+    }
+
+    private ArrayList<Module> getAddedModules() {
+        Intent intent = getIntent();
+        String index = intent.getStringExtra("Index");
+        String department = "Fresher";
+
+        UserDBHelper dbHelper = new UserDBHelper(getApplicationContext());
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+
+        String[] projection = {
+                UserReaderDB.UserEntry.COLUMN_NAME_MODULE_NAME,
+                UserReaderDB.UserEntry.COLUMN_NAME_MODULE_CODE,
+                UserReaderDB.UserEntry.COLUMN_NAME_CREDITS
+        };
+
+        String selection = UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER + " = ? AND " +
+                UserReaderDB.UserEntry.COLUMN_NAME_DEPARTMENT + " = ? AND " +
+                UserReaderDB.UserEntry.COLUMN_NAME_INDEX + " =?";
+        String[] selectionArgs = {String.valueOf(1), department, index};
+        String sortOrder = UserReaderDB.UserEntry.COLUMN_NAME_MODULE_NAME + " DESC";
+
+        Cursor cursor = database.query(
+                UserReaderDB.UserEntry.TABLE_NAME_MODULE,
+                projection,
+                selection,
+                selectionArgs,
+                null,
+                null,
+                sortOrder
+        );
+
+        ArrayList<Module> addedModules = new ArrayList<>();
+
+        while (cursor.moveToNext()) {
+            String moduleName = cursor.getString(
+                    cursor.getColumnIndexOrThrow(UserReaderDB.UserEntry.COLUMN_NAME_MODULE_NAME)
+            );
+
+            String moduleCode = cursor.getString(
+                    cursor.getColumnIndexOrThrow(UserReaderDB.UserEntry.COLUMN_NAME_MODULE_CODE)
+            );
+
+            double credits = cursor.getDouble(
+                    cursor.getColumnIndexOrThrow(UserReaderDB.UserEntry.COLUMN_NAME_CREDITS)
+            );
+            Module module = new Module(moduleCode, moduleName, credits);
+            addedModules.add(module);
+        }
+        cursor.close();
+        return addedModules;
     }
 }
