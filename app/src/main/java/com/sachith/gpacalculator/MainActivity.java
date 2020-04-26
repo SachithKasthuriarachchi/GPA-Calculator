5bb68eeb7c2838c10a1c4653ed6ed5d9ac8981a8
SachithKasthuriarachchi <sac.nil.kas@gmail.com>
2020-04-26 14:19:46 +0530

    Correcting the upgrading error by checking if there is the newly added table.

--- app/src/main/java/com/sachith/gpacalculator/MainActivity.java
+++ app/src/main/java/com/sachith/gpacalculator/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sachith.gpacalculator;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -10,8 +11,22 @@ import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String SQL_CREATE_ENTRIES_MODULE =
+            "CREATE TABLE IF NOT EXISTS " + UserReaderDB.UserEntry.TABLE_NAME_MODULE + " (" +
+                    UserReaderDB.UserEntry._ID + " INTEGER PRIMARY KEY," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_INDEX + " TEXT," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_DEPARTMENT + " TEXT," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_SEMESTER + " INTEGER," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_MODULE_CODE + " TEXT," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_MODULE_NAME + " TEXT," +
+                    UserReaderDB.UserEntry.COLUMN_NAME_CREDITS + " REAL)";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        UserDBHelper dbHelper = new UserDBHelper(getApplicationContext());
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        database.execSQL(SQL_CREATE_ENTRIES_MODULE);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
